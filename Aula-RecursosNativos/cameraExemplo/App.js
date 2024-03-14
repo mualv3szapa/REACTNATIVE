ca3436e10574381fe0dafb020c2f43b6acf285e4
Murilo Alves Zapiello <54750759805@info.local>
2024-03-14 10:44:57 -0300

    finalizado

--- Aula-RecursosNativos/cameraExemplo/App.js
+++ Aula-RecursosNativos/cameraExemplo/App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import {
+  Alert,
   Image,
   Modal,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
+  Video,
 } from "react-native";
 import { Camera, CameraType } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
 import { useEffect, useState, useRef } from "react";
 import { FontAwesome } from "@expo/vector-icons";
 
@@ -15,13 +18,35 @@ export default function App() {
   const cameraRef = useRef(null);
 
   const [photo, setPhoto] = useState(null);
+  const [recording, setRecording] = useState(false);
+  const [videoUri, setVideoUri] = useState(null);
   const [openModal, setOpenModal] = useState(false);
   const [tipoCamera, setTipoCamera] = useState(CameraType.front);
+
+  const startRecording = async () => {
+    setRecording(true);
+    const { uri } = await cameraRef.recordAsync();
+    setVideoUri(uri);
+    setRecording(false);
+  };
+
+  const stopRecording = async () => {
+    if (recording) {
+      await cameraRef.stopRecording();
+      setRecording(false);
+    }
+  };
 
   useEffect(() => {
     (async () => {
       const { status: cameraStatus } =
         await Camera.requestCameraPermissionsAsync();
+
+      const { status: microphoneStatus } =
+        await Camera.requestMicrophonePermissionsAsync();
+
+      const { status: mediaStatus } =
+        await MediaLibrary.requestPermissionsAsync();
     })();
   }, []);
 
@@ -37,6 +62,24 @@ export default function App() {
     }
   }
 
+  function ClearPhoto() {
+    setPhoto(null);
+
+    setOpenModal(false);
+  }
+
+  async function SavePhoto() {
+    if (photo) {
+      await MediaLibrary.createAssetAsync(photo)
+        .then(() => {
+          Alert.alert("Sucesso", "Foto salva na galeria");
+        })
+        .catch((error) => {
+          alert("Erro ao processar foto");
+        });
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Camera
@@ -44,7 +87,8 @@ export default function App() {
         style={styles.camera}
         ratio="16:9"
         type={tipoCamera}
-        // zoom={0.9}
+        // zoom={0.5}
+        // autoFocus="on"
       >
         <View style={styles.ViewFlip}>
           <TouchableOpacity
@@ -69,6 +113,14 @@ export default function App() {
         <FontAwesome name="camera" size={23} color="#fff" />
       </TouchableOpacity>
 
+      <TouchableOpacity
+        onPress={recording ? stopRecording : startRecording}
+        style={{ padding: 10, backgroundColor: recording ? "red" : "green" }}
+      >
+        <Text>{recording ? "Parar" : "Gravar"}</Text>
+      </TouchableOpacity>
+
+      
 
       <Modal animationType="slide" transparent={false} visible={openModal}>
         <View
@@ -87,6 +139,22 @@ export default function App() {
             style={{ width: "100%", height: 500, borderRadius: 15 }}
             source={{ uri: photo }}
           />
+
+          <View style={{ margin: 15, flexDirection: "row" }}>
+            <TouchableOpacity
+              style={styles.btnCancel}
+              onPress={() => ClearPhoto()}
+            >
+              <FontAwesome name="trash" size={35} color="green" />
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={styles.btnCancel}
+              onPress={() => SavePhoto()}
+            >
+              <FontAwesome name="save" size={35} color="#121212" />
+            </TouchableOpacity>
+          </View>
         </View>
       </Modal>
     </View>
@@ -127,5 +195,19 @@ const styles = StyleSheet.create({
     backgroundColor: "#121212",
     justifyContent: "center",
     alignItems: "center",
+  },
+  btnCancel: {
+    padding: 20,
+    borderRadius: 15,
+    backgroundColor: "transparent",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  btnCancel: {
+    padding: 20,
+    borderRadius: 15,
+    backgroundColor: "transparent",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
